e1959cc3ee6f15ef908bb0ce01dc772b0bcd43e0
Akash Mishra <akashmishra0389@gmail.com>
2025-07-21 00:19:04 +0530

    setup sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
     </div>
   )
 }
